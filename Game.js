e73d4887150380ba5495be6494478b0da2d6112a
aarya1497 <74665578+aarya1497@users.noreply.github.com>
2021-08-20 00:14:36 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,111 @@
+class Game {
+  constructor(){
+
+  }
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
+      form = new Form()
+      form.display();
+    }
+
+    car1 = createSprite(100,200);
+    car1.addImage("covid",car1_img);
+    car2 = createSprite(300,200);
+    car2.addImage("vaccin",car2_img);
+    
+  }
+
+  play(){
+    form.hide();
+    
+    Player.getPlayerInfo();
+    player.getendCount();
+
+    if(allPlayers !== undefined){
+      background(rgb(198,135,103));
+      image(track, 0,-displayHeight*4,displayWidth, displayHeight*5);
+      
+      //var display_position = 100;
+      
+      //index of the array
+      var index = 0;
+
+      //x and y position of the cars
+      var x = 175 ;
+      var y;
+
+      for(var plr in allPlayers){
+        //add 1 to the index for every loop
+        index = index + 1 ;
+
+        //position the cars a little away from each other in x direction
+        x = x + 200;
+        //use data form the database to display the cars in y direction
+        y = displayHeight - allPlayers[plr].distance;
+        cars[index-1].x = x;
+        cars[index-1].y = y;
+
+        if (index === player.index){
+          fill("black");
+          ellipse(x,y,60,60);
+          camera.position.x = displayWidth/2;
+          camera.position.y = cars[index-1].y;
+        }
+       
+        //textSize(15);
+        //text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+      }
+
+    }
+
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance +=10
+      player.update();
+    }
+
+    if(player.distance > 3860){
+      gameState = 2;
+      player.rank += 1;
+      Player.updateendCount(player.rank);
+    }
+   
+    drawSprites();
+  }
+
+  end(){
+   
+  background("lightblue");
+ 
+  var t = createElement('h2');
+  t.html("Game over!!!");
+  t.position(displayWidth/2-100,displayHeight/2);
+  
+  var t1 = createElement('h2');
+  t1.html(player.rank);
+  t1.position(displayWidth/2-100,displayHeight/2 + 100);
+  
+  
+    
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,41 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2;
+
+var track, car1_img, car2_img;
+
+function preload(){
+  ground = loadImage("/images/game bg.png");
+  car1 = loadImage("/images/covid.png");
+  car2 = loadImage("/images/vaccin.png");
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 2){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState === 2){
+    game.end();
+  }
+}
